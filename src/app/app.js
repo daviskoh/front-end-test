b0e633aea1b6f9e4b18ebe10ef5779d351fc8f8e
daviskoh <koh.davis.0@gmail.com>
2015-01-15 11:44:05 -0500

    create basic app archi concept

---
+++ src/app/app.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    // NOTE: due to small size of app, all ng components will be defined on core pacificOcean mod
+    // when apps gets larger, will need to use sub-module architecture & inject into core below
+    angular.module('pacificOcean', [
+        'ui.router'
+    ])
+        .config(configRoutes);
+
+    // @ngInject
+    function configRoutes($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'app/views/home.tpl.html'
+            });
+
+        $urlRouterProvider.otherwise('/');
+    }
+})();
