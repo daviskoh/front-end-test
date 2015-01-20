dbc6aa0aa5969f6ff38a5bf27cabe2ec5dc425dc
daviskoh <koh.davis.0@gmail.com>
2015-01-20 15:55:04 -0500

    update history text font

--- src/app/templates.js
+++ src/app/templates.js
@@ -1,7 +1,7 @@
 angular.module('pacificOcean.templates', []).run(['$templateCache', function($templateCache) {
   "use strict";
   $templateCache.put("app/views/history.tpl.html",
-    "<div class=\"history-container\"><div class=\"column\">History <a class=\"close-history\" href=\"\" ui-sref=\"home()\">X</a></div><div class=\"column wikipedia-text\"><div ng-repeat=\"paragraph in vm.history.content\"><p>{{ paragraph }}</p></div></div></div>");
+    "<div class=\"history-container\"><div class=\"column\">History <a class=\"close-history\" href=\"\" ui-sref=\"home()\">X</a></div><div class=\"column wikipedia-text\"><div ng-repeat=\"paragraph in vm.history.content\"><p class=\"wiki-text\">{{ paragraph }}</p></div></div></div>");
   $templateCache.put("app/views/home.tpl.html",
     "<navigation><a href=\"\" class=\"nav-link\">Sea Otters</a> <a href=\"\" class=\"nav-link\">Killer Whales</a> <a href=\"\" class=\"nav-link\">Viper Fish</a></navigation><div class=\"content\"><div class=\"top\"><p class=\"pacific-ocean-title\">Pacific Ocean</p><div class=\"viper-fish-container\"><img class=\"viper-fish-img\" src=\"{{ vm.images.bigFish }}\"></div></div><div class=\"bottom\"><div class=\"bottom-container\"><div class=\"square-image-container square-image-container-left\"><img class=\"square-image\" src=\"{{ vm.images.whale }}\"></div><div class=\"square-image-container square-image-container-right\"><img class=\"square-image\" src=\"{{ vm.images.smallFish }}\"></div><a class=\"footer\" href=\"\" ui-sref=\"history()\"><div class=\"history\">History</div></a></div></div><div ui-view></div></div>");
   $templateCache.put("app/views/navigation.tpl.html",
